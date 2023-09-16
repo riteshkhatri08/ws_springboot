f414bffb4955920c240edee0cfb92ef4152e02d1
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-16 18:04:15 +0530

    Added new project - Rest Client

---
+++ restclient/src/test/java/com/ritesh/springboot/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.springboot.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
