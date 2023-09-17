47257f46682a49806712cb841e85150b44198efd
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-18 01:54:08 +0530

    break down Main Application

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/TacoCloudApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// ? Above annotation is combination of below 3 annotations
+// * @SpringBootCongfiguration - just a special form of @Configuration
+// * @EnableAutoConfiugration - Tells spring boot to autoconfigure any
+// components that it THINKS we'll need
+// * @ComponentScan - Enables Component scanning- scans annotated classes
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
+		// ? Configuration class and cli args are passed
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
