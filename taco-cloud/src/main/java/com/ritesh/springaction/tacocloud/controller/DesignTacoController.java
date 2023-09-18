c96fbb2b4c4e087e11a2e0b460d3dc0e3c9e787b
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-18 15:06:43 +0530

    Added post method to Design controller and OrderController to process orders

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/controller/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -27,6 +28,14 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco ,  @ModelAttribute TacoOrder tacoOrder){
+     
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 
     @ModelAttribute // ? @ModelAttribute methods are invoked before the controller methods annotated with @RequestMapping are invoked.
