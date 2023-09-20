cb8bcd6cb514b47fa88eef2bab691db9d30ce702
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-20 23:26:00 +0530

    Loggers to check if which thread served the request while bootstrapping

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/controller/HomeController.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/controller/HomeController.java
@@ -3,11 +3,15 @@ package com.ritesh.springaction.tacocloud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        log.debug(Thread.currentThread() + " ::::: HAS SERVED A REQUEST");
         return "home";
     }
 
