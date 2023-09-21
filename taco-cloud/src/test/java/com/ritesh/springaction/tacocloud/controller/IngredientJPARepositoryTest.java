a216d9f04fe88fed0751a31dd4543983e44342de
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-21 23:20:17 +0530

    Added Test for JPA Repository and removed Test for JDBC repo

---
+++ taco-cloud/src/test/java/com/ritesh/springaction/tacocloud/controller/IngredientJPARepositoryTest.java
@@ -0,0 +1,35 @@
+package com.ritesh.springaction.tacocloud.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ritesh.springaction.tacocloud.dao.IngredientJPARepository;
+import com.ritesh.springaction.tacocloud.model.Ingredient;
+
+@SpringBootTest
+public class IngredientJPARepositoryTest {
+
+    @Autowired
+    IngredientJPARepository repo;
+
+    @Test
+    void testFindById() {
+
+        Iterable<Ingredient> ingredients = repo.findAll();
+        System.out.println("LIST OF INGRDIENTS IN DB");
+        ingredients.forEach(System.out::println);
+
+        // Optional<Ingredient> ingredient = repo.findById("FLTO");
+
+        // assertTrue(ingredient.isPresent());
+        // assertEquals("FLTO", ingredient.get().getId());
+        // assertEquals(Ingredient.Type.WRAP, ingredient.get().getType());
+
+    }
+}
--- taco-cloud/src/test/java/com/ritesh/springaction/tacocloud/controller/IngredientRepositoryTest.java
+++ taco-cloud/src/test/java/com/ritesh/springaction/tacocloud/controller/IngredientRepositoryTest.java
@@ -5,28 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
+import com.ritesh.springaction.tacocloud.Type;
 import com.ritesh.springaction.tacocloud.data.repository.IngredientRepository;
 import com.ritesh.springaction.tacocloud.model.Ingredient;
 
-@SpringBootTest
+// @SpringBootTest
 public class IngredientRepositoryTest {
 
-    @Autowired
+    // @Autowired
     IngredientRepository repo;
 
-@Test
-void testFindById(){
+    // @Test
+    void testFindById() {
 
-   Optional<Ingredient> ingredient =  repo.findById("FLTO");
+        Optional<Ingredient> ingredient = repo.findById("FLTO");
 
-   assertTrue(ingredient.isPresent());
-   assertEquals("FLTO",ingredient.get().getId());
-   assertEquals(Ingredient.Type.WRAP , ingredient.get().getType());
+        assertTrue(ingredient.isPresent());
+        assertEquals("FLTO", ingredient.get().getId());
+        assertEquals(Type.WRAP, ingredient.get().getType());
 
-}
+    }
 
 }
