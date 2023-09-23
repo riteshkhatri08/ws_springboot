f2f67620a8b32483941ee669284b2b9aa84ec884
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-23 18:09:21 +0530

    Tested Bootstrap using initializing bean

---
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/bootstrap/BootstrapInitiailzingBean.java
@@ -0,0 +1,27 @@
+package com.ritesh.springaction.tacocloud.bootstrap;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class BootstrapInitiailzingBean implements InitializingBean {
+
+    @Override
+    @PostConstruct
+    public void afterPropertiesSet() throws Exception {
+        log.debug("Inside BootstrapInitiailzingBean: Sleeping for 30 Seconds");
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+         log.debug("Inside BootstrapInitiailzingBean: WOKE UP after 30 Seconds");
+
+    }
+
+}
