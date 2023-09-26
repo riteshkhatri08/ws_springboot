87ee62f6e5f9247fec3ceb42736ca567eba37fc5
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-27 00:42:52 +0530

    Configured In Memory User Detais, Service Bean

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.ritesh.springaction.tacocloud.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import  org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
@@ -24,5 +33,20 @@ public class SecurityConfig {
          */
     }
 
-  
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        // Adding this config to add users to this default bean
+        List<UserDetails> usersList = new ArrayList<>();
+        // Add users
+        usersList.add(new User("dj_bobby_deol", encoder.encode("strongpassword"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User("selmon_bhai", encoder.encode("footpath"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        // Return a manager that stores these users in memory
+        // Below class implements UserDetailsService
+        return new InMemoryUserDetailsManager(usersList);
+    }
+
 }
