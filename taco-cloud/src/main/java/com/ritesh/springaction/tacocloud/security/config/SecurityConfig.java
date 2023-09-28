2d36dda9da8dcd1bdd84d6a5795e15a87d27c01d
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-28 22:59:51 +0530

    code clean up

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/config/SecurityConfig.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/config/SecurityConfig.java
@@ -1,19 +1,8 @@
 package com.ritesh.springaction.tacocloud.security.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import com.ritesh.springaction.tacocloud.security.dao.UserRepository;
 
