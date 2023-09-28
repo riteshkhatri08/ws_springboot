1ade8259edbd1ed5c3bd00ea21f5d9590dc595d3
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-09-28 23:33:41 +0530

    Added Password encoder bean

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/config/SecurityConfig.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/security/config/SecurityConfig.java
@@ -3,27 +3,29 @@ package com.ritesh.springaction.tacocloud.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ritesh.springaction.tacocloud.security.dao.UserRepository;
 
 @Configuration
 public class SecurityConfig {
 // ? Commenting out this bena as we now have a custom bean that can fetch users from db 
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     // ? This passwordEncoder bean can be used to
-    //     // ? Add new user
-    //     // ? Authenticate users during login
-    //     return new BCryptPasswordEncoder();
-    //     // ? Other encoders
-    //     /*
-    //      * BCryptPasswordEncoder—Applies bcrypt strong hashing encryption
-    //      * NoOpPasswordEncoder—Applies no encoding
-    //      * Pbkdf2PasswordEncoder—Applies PBKDF2 encryption
-    //      * SCryptPasswordEncoder—Applies Scrypt hashing encryption
-    //      * StandardPasswordEncoder—Applies SHA-256 hashing encryption
-    //      */
-    // }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // ? This passwordEncoder bean can be used to
+        // ? Add new user
+        // ? Authenticate users during login
+        return new BCryptPasswordEncoder();
+        // ? Other encoders
+        /*
+         * BCryptPasswordEncoder—Applies bcrypt strong hashing encryption
+         * NoOpPasswordEncoder—Applies no encoding
+         * Pbkdf2PasswordEncoder—Applies PBKDF2 encryption
+         * SCryptPasswordEncoder—Applies Scrypt hashing encryption
+         * StandardPasswordEncoder—Applies SHA-256 hashing encryption
+         */
+    }
 
     // ? Commenting out this bena as we now have a custom bean that can fetch users from db 
     // @Bean
