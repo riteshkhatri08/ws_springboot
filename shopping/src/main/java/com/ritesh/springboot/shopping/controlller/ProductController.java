63c7b1d3d1f027551a0cd5ba261f5880e934aa0b
riteshkhatri08 <riteshkhatri1512@gmail.com>
2023-10-01 03:39:48 +0530

    Added products context path mapping at class level

--- shopping/src/main/java/com/ritesh/springboot/shopping/controlller/ProductController.java
+++ shopping/src/main/java/com/ritesh/springboot/shopping/controlller/ProductController.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ritesh.springboot.shopping.entities.Product;
 import com.ritesh.springboot.shopping.service.ProductService;
 
-@RestController
+@RestController()
+@RequestMapping(value="/products")
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
-    @GetMapping(value="/products")
+    // @GetMapping(value="/products")
+    @GetMapping
     public List<Product> getProducts() {
         return productService.getAllProducts();
     }
