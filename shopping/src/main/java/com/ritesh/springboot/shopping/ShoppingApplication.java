b043559cc568a5cb1a62e3c2ba9b74232f256ab2
riteshkhatri08 <riteshkhatri1512@gmail.com>
2023-10-01 02:58:04 +0530

    Moved App Init logic to main class

--- shopping/src/main/java/com/ritesh/springboot/shopping/ShoppingApplication.java
+++ shopping/src/main/java/com/ritesh/springboot/shopping/ShoppingApplication.java
@@ -1,13 +1,34 @@
 package com.ritesh.springboot.shopping;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ritesh.springboot.shopping.dao.ProductRepository;
+import com.ritesh.springboot.shopping.entities.Product;
 
 @SpringBootApplication
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner(ProductRepository productRepository) {
+		return args -> {
+			List<Product> products = List.of(
+					new Product("Football", BigDecimal.valueOf(25.00)),
+					new Product("Volleyball", BigDecimal.valueOf(500.00)),
+					new Product("Basketball", BigDecimal.valueOf(1200.00)));
+
+			productRepository.saveAll(products);
+
+		};
 	}
 
 }
