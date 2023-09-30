776cfb5333d4f342d90b68912dc3ec4e10a540f0
riteshkhatri08 <riteshkhatri1512@gmail.com>
2023-09-30 17:21:09 +0530

    Added default user dusring application start up

--- taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/bootstrap/CZApplicationRunner.java
+++ taco-cloud/src/main/java/com/ritesh/springaction/tacocloud/bootstrap/CZApplicationRunner.java
@@ -1,14 +1,16 @@
 package com.ritesh.springaction.tacocloud.bootstrap;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.availability.ApplicationAvailability;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.ritesh.springaction.tacocloud.security.dao.UserRepository;
+import com.ritesh.springaction.tacocloud.security.entity.User;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +22,12 @@ public class CZApplicationRunner
 
     @Autowired
     ApplicationAvailability availabilityProvider;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -40,25 +48,40 @@ public class CZApplicationRunner
         // sleep..");
         // log.debug("Creating Caches from DB for table - INGREDIENT");
 
+        addDefaultUserToRepository();
+
+    }
+
+    private void addDefaultUserToRepository() {
+
         // do nothing
-    }
+        // Add a default user at startup for testing
+        User defualtUser = new User("ritesh", passwordEncoder.encode("password"), "riteshkhatri", "street", "city",
+                "state", "452001", "987543210");
+        log.info("\n\n\n Adding default user with username = 'ritesh' and password = 'password'\n\n\n");
 
-    @PostConstruct
-    public void init() {
-        log.debug(Thread.currentThread() + " ::::: SLEEPING for 1 seconds ");
-        try {
-            // ?  we can use @PostConstruct on a method and that method will be called after a bean has been initialized .. s
-            // ? here we can performDB based operation like populating a cache from DB
-            log.debug("liveness state =" + availabilityProvider.getLivenessState());
-            log.debug("Readiness State = " + availabilityProvider.getReadinessState());
-
-            Thread.sleep(1); // for testing purpose only
-        } catch (InterruptedException e) {
-
-            e.printStackTrace();
-        }
-        log.debug(Thread.currentThread() + " ::::: Woke up after a 30 second sleep ");
+        userRepository.save(defualtUser);
 
     }
+
+    // @PostConstruct
+    // public void init() {
+    // log.debug(Thread.currentThread() + " ::::: SLEEPING for 1 seconds ");
+    // try {
+    // // ? we can use @PostConstruct on a method and that method will be called
+    // after a bean has been initialized .. s
+    // // ? here we can performDB based operation like populating a cache from DB
+    // log.debug("liveness state =" + availabilityProvider.getLivenessState());
+    // log.debug("Readiness State = " + availabilityProvider.getReadinessState());
+
+    // Thread.sleep(1); // for testing purpose only
+    // } catch (InterruptedException e) {
+
+    // e.printStackTrace();
+    // }
+    // log.debug(Thread.currentThread() + " ::::: Woke up after a 30 second sleep
+    // ");
+
+    // }
 
 }
